690a3273b4b2ff3a788a752297afe84d6ed081cb
b-hernandez <43477114+b-hernandez@users.noreply.github.com>
2024-03-01 12:31:52 -0600

    Added Threading
    Working V1 Version

--- AgentLicenseLookup.py
+++ AgentLicenseLookup.py
@@ -1,10 +1,12 @@
 import sys
+import time
 
 sys.path.append('C:\\PA-Functions')
 
 import json
 import pathlib
 from datetime import datetime
+import threading
 
 from web_functions import WebFunctions
 from db_functions import DBCalls
@@ -71,17 +73,109 @@ def setup_search_sircon(browser):
 
 def process_google_sheet_data(google_sheet_link):
     # google_sheet_data = google.get_all_data_from_spreadsheet(google_sheet_link, cred.google_sheet_agent_license_status, json_format=True)
-
     agent_google_sheet_data = db_functions.get_agent_license_status([None])
 
-    google.delete_data_from_spreadsheet(google_sheet_link, cred.google_sheet_agent_license_status)
+    if agent_google_sheet_data is not None and agent_google_sheet_data != []:
 
-    data_list = []
+        data_list = []
 
-    for data in agent_google_sheet_data:
-        data_list.append(list(data))
+        for data in agent_google_sheet_data:
+            data_list.append(list(data))
 
-    google.add_google_sheet_data(google_sheet_link, data_list, True,  cred.google_sheet_agent_license_status)
+        if data_list is not None and len(data_list) != 0:
+            google.delete_data_from_spreadsheet(google_sheet_link, cred.google_sheet_agent_license_status)
+            google.add_google_sheet_data(google_sheet_link, data_list, True, cred.google_sheet_agent_license_status)
+
+    return None
+
+
+def process_google_sheet_data_individual(google_sheet_link):
+    # google_sheet_data = google.get_all_data_from_spreadsheet(google_sheet_link, cred.google_sheet_agent_license_status, json_format=True)
+    agent_google_sheet_data = db_functions.get_agent_individual_status([None])
+
+    if agent_google_sheet_data is not None and agent_google_sheet_data != []:
+
+        data_list = []
+
+        for data in agent_google_sheet_data:
+            data_list.append(list(data))
+
+        if data_list is not None and len(data_list) != 0:
+            google.delete_data_from_spreadsheet(google_sheet_link, cred.google_sheet_agent_lookup_individual)
+            google.add_google_sheet_data(google_sheet_link, data_list, True, cred.google_sheet_agent_lookup_individual)
+
+    return None
+
+
+def process_google_sheet_data_appointment(google_sheet_link):
+    # google_sheet_data = google.get_all_data_from_spreadsheet(google_sheet_link, cred.google_sheet_agent_license_status, json_format=True)
+    agent_google_sheet_data = db_functions.get_agent_appointment_status([None])
+    if agent_google_sheet_data is not None and agent_google_sheet_data != []:
+
+        data_list = []
+
+        for data in agent_google_sheet_data:
+            data_list.append(list(data))
+
+        if data_list is not None and len(data_list) != 0:
+            google.delete_data_from_spreadsheet(google_sheet_link, cred.google_sheet_agent_lookup_appointment)
+            google.add_google_sheet_data(google_sheet_link, data_list, True, cred.google_sheet_agent_lookup_appointment)
+
+    return None
+
+
+def thread_function(agent_lookup_data, google_sheet_link, thread_number):
+    browser = None
+    try:
+        browser = load_sircon()
+
+        browser = setup_search_sircon(browser)
+
+        browser = web_functions.sircon_select_individual_search(browser)
+
+        thread_index = 1
+
+        for agent in agent_lookup_data:
+            print("Thread # " + str(thread_number) + " Processing : " + str(thread_index) + " out of " + str(
+                len(agent_lookup_data)))
+
+            if thread_index % 25 == 0:
+                process_google_sheet_data(google_sheet_link)
+                process_google_sheet_data_individual(google_sheet_link)
+                process_google_sheet_data_appointment(google_sheet_link)
+
+            browser = web_functions.sircon_add_license_search_info(browser, agent["LicenseNumber"])
+
+            # At this point it should be on the screen
+            browser, individual_data, data_returned = web_functions.sircon_get_individual_date(browser)
+
+            if data_returned:
+                # Add a try here since if succesful this will break
+                browser, appointment_data = web_functions.sircon_agency_lookup(browser, "Redpoint County Mutual Insurance Company")
+
+                db_functions.insert_agent_websnapshot(
+                    [json.dumps(individual_data), json.dumps(appointment_data), agent["AgentImportId"]])
+
+                if appointment_data == [] or individual_data == []:
+                    db_functions.update_agent_license_processing([agent["AgentImportId"], True, False, None])
+                else:
+                    db_functions.update_agent_license_processing([agent["AgentImportId"], True, data_returned, None])
+
+                log_information(note="Thread # " + str(thread_number) + ' Success DateBatchId : ' + datetime.now().strftime('%Y%m%d') + ' Agent Import Id : ' + str(
+                    agent["AgentImportId"]), failure_bit=0)
+
+            else:
+                db_functions.update_agent_license_processing([agent["AgentImportId"], True, data_returned, None])
+
+                log_information(note="Thread # " + str(thread_number) + ' Failure DateBatchId : ' + datetime.now().strftime('%Y%m%d') + ' Agent Import Id : ' + str(
+                    agent["AgentImportId"]), failure_bit=1)
+
+            thread_index += 1
+    except Exception as e:
+        print("Error occurred during processing " + str(e))
+    finally:
+        if browser:
+            browser.quit()
 
     return None
 
@@ -97,69 +191,56 @@ def main():
 
     # function to get claims that will
     oracle_json_data = db_functions.get_pulse_agent_lookup()
-    db_functions.insert_agent_import([oracle_json_data, "Pulse"])
+    if oracle_json_data != [] and oracle_json_data is not None:
+        db_functions.insert_agent_import([oracle_json_data, "Pulse"])
 
     agent_lookup_data = db_functions.get_agent_import_data()
     db_functions.insert_agent_license_processing(None)
 
-    # agent_google_sheet_data = list(agent_google_sheet_data)
-
     process_google_sheet_data(google_sheet_link)
+    process_google_sheet_data_individual(google_sheet_link)
+    process_google_sheet_data_appointment(google_sheet_link)
 
     # for agent in agent_google_sheet_data:
     #     agent_data = list(agent)
     #     google.update_google_sheet_row_by_id(google_sheet_link, agent_data[0], agent_data[1:], True, cred.google_sheet_agent_license_status)
 
-    browser = load_sircon()
-
-    browser = setup_search_sircon(browser)
-
-    browser = web_functions.sircon_select_individual_search(browser)
-
-    index = 1
-
-    for agent in agent_lookup_data:
-        print("Processing : " + str(index) + " out of " + str(len(agent_lookup_data)))
-
-        if index % 25 == 0:
-            process_google_sheet_data(google_sheet_link)
-
-        browser = web_functions.sircon_add_license_search_info(browser, agent["LicenseNumber"])
-
-        # At this point it should be on the screen
-        browser, individual_data, data_returned = web_functions.sircon_get_individual_date(browser)
-
-        if data_returned:
-
-            browser, appointment_data = web_functions.sircon_agency_lookup(browser, "Redpoint County Mutual Insurance Company")
-
-            db_functions.insert_agent_websnapshot(
-                [json.dumps(individual_data), json.dumps(appointment_data), agent["AgentImportId"]])
-
-            db_functions.update_agent_license_processing([agent["AgentImportId"], True, data_returned, None])
-
-            log_information(note='Success Agent Import Id ' + datetime.now().strftime('%Y%m%d') + ' : ' + str(
-                agent["AgentImportId"]), failure_bit=0)
-
+    if len(agent_lookup_data) != 0:
+        if len(agent_lookup_data) >= 4:
+            list_size = len(agent_lookup_data) // 4
         else:
-            db_functions.update_agent_license_processing([agent["AgentImportId"], True, data_returned, None])
+            list_size = agent_lookup_data
 
-            log_information(note='Failure Agent Import Id ' + datetime.now().strftime('%Y%m%d') + ' : ' + str(
-                agent["AgentImportId"]), failure_bit=1)
+        agent_chunks = [agent_lookup_data[i:i + list_size] for i in range(0, len(agent_lookup_data), list_size)]
 
-        # Have to comment out due to google quota limit need to do a full refresh only once and a while
-        # google_sheet_data = db_functions.get_agent_processed_individual_by_id([agent["AgentImportId"]])
-        # for google_data in google_sheet_data:
-        #     data_list = list(google_data)
-        #
-        #     print(str(data_list[0]))
-        #
-        #     google.update_google_sheet_row_by_id(google_sheet_link, data_list[0], data_list[1:], True,
-        #                                          cred.google_sheet_agent_lookup_individual)
-        #
-        #     print("should be updated")
+        threads = []
+        for agent_list in agent_chunks:
+            thread = threading.Thread(target=thread_function, args=(agent_list, google_sheet_link, len(threads)+1,))
+            threads.append(thread)
+            thread.start()
+            time.sleep(20)
 
-        index += 1
+        for thread in threads:
+            thread.join()
+
+        process_google_sheet_data(google_sheet_link)
+        process_google_sheet_data_individual(google_sheet_link)
+        process_google_sheet_data_appointment(google_sheet_link)
+    else:
+        print("No data")
+
+
+    # Have to comment out due to google quota limit need to do a full refresh only once and a while
+    # google_sheet_data = db_functions.get_agent_processed_individual_by_id([agent["AgentImportId"]])
+    # for google_data in google_sheet_data:
+    #     data_list = list(google_data)
+    #
+    #     print(str(data_list[0]))
+    #
+    #     google.update_google_sheet_row_by_id(google_sheet_link, data_list[0], data_list[1:], True,
+    #                                          cred.google_sheet_agent_lookup_individual)
+    #
+    #     print("should be updated")
 
 
 if __name__ == "__main__":
